53dfa447a8130beb7fa404cae5ca8af9330b4b56
Jacob Van Noort <jakevn@gmail.com>
2015-04-27 21:21:23 -0400

    Initial

---
+++ bitbuf.go
@@ -0,0 +1,219 @@
+package bitbuf
+
+type bitbuf struct {
+	buf  []byte
+	pos  uint32
+	size uint32
+}
+
+func NewBitbuf(size uint32) *bitbuf {
+	return &bitbuf{make([]byte, size), 0, size * 8}
+}
+
+func (b *bitbuf) Size() uint32 {
+	return b.size
+}
+
+func (b *bitbuf) ByteSize() uint32 {
+	return b.size / 8
+}
+
+func (b *bitbuf) Pos() uint32 {
+	return b.pos
+}
+
+func (b *bitbuf) SetPos(pos uint32) {
+	if pos > b.size {
+		b.pos = b.size
+	} else {
+		b.pos = pos
+	}
+}
+
+func (b *bitbuf) Reset() {
+	b.pos = 0
+	for i, _ := range b.buf {
+		b.buf[i] = 0
+	}
+}
+
+func (b *bitbuf) CanWrite(bits uint32) bool {
+	return b.pos+bits <= b.size
+}
+
+func (b *bitbuf) CanRead(bits uint32) bool {
+	return b.pos+bits <= b.size
+}
+
+func (b *bitbuf) WriteBool(value bool) {
+	if value {
+		b.writeByte(1, 1)
+	} else {
+		b.writeByte(0, 1)
+	}
+}
+
+func (b *bitbuf) ReadBool() bool {
+	return b.readByte(1) == 1
+}
+
+func (b *bitbuf) WriteByte(value byte) {
+	b.WriteBytePart(value, 8)
+}
+
+func (b *bitbuf) WriteBytePart(value byte, bits uint32) {
+	b.writeByte(value, bits)
+}
+
+func (b *bitbuf) ReadByte() byte {
+	return b.ReadBytePart(8)
+}
+
+func (b *bitbuf) ReadBytePart(bits uint32) byte {
+	return b.readByte(bits)
+}
+
+func (b *bitbuf) WriteUint16(value uint16) {
+	b.WriteUint16Part(value, 16)
+}
+
+func (b *bitbuf) WriteUint16Part(value uint16, bits uint32) {
+	if bits <= 8 {
+		b.writeByte(byte(value&0xFF), bits)
+	} else {
+		b.writeByte(byte(value&0xFF), 8)
+		b.writeByte(byte(value>>8), bits-8)
+	}
+}
+
+func (b *bitbuf) ReadUint16() uint16 {
+	return b.ReadUint16Part(16)
+}
+
+func (b *bitbuf) ReadUint16Part(bits uint32) uint16 {
+	if bits <= 8 {
+		return uint16(b.readByte(bits))
+	} else {
+		return uint16(b.readByte(8) | (b.readByte(bits-8) << 8))
+	}
+}
+
+func (b *bitbuf) WriteInt16(value int16) {
+	b.WriteInt16Part(value, 16)
+}
+
+func (b *bitbuf) WriteInt16Part(value int16, bits uint32) {
+	b.WriteUint16Part(uint16(value), bits)
+}
+
+func (b *bitbuf) ReadInt16() int16 {
+	return int16(b.ReadUint16Part(16))
+}
+
+func (b *bitbuf) ReadInt16Part(bits uint32) int16 {
+	return int16(b.ReadUint16Part(bits))
+}
+
+func (b *bitbuf) WriteUint32Part(value uint32, bits uint32) {
+	w := byte(value >> 0)
+	x := byte(value >> 8)
+	y := byte(value >> 16)
+	z := byte(value >> 24)
+
+	switch (bits + 7) / 8 {
+	case 1:
+		b.writeByte(w, bits)
+		break
+	case 2:
+		b.writeByte(w, 8)
+		b.writeByte(x, bits-8)
+		break
+	case 3:
+		b.writeByte(w, 8)
+		b.writeByte(x, 8)
+		b.writeByte(y, bits-16)
+		break
+	case 4:
+		b.writeByte(w, 8)
+		b.writeByte(x, 8)
+		b.writeByte(y, 8)
+		b.writeByte(z, bits-24)
+		break
+	}
+}
+
+func (b *bitbuf) ReadUint32Part(bits uint32) uint32 {
+	var w, x, y, z int32
+	w, x, y, z = 0, 0, 0, 0
+
+	switch (bits + 7) / 8 {
+	case 1:
+		w = int32(b.readByte(bits))
+		break
+	case 2:
+		w = int32(b.readByte(8))
+		x = int32(b.readByte(bits - 8))
+		break
+	case 3:
+		w = int32(b.readByte(8))
+		x = int32(b.readByte(8))
+		y = int32(b.readByte(bits - 16))
+		break
+	case 4:
+		w = int32(b.readByte(8))
+		x = int32(b.readByte(8))
+		y = int32(b.readByte(8))
+		z = int32(b.readByte(bits - 24))
+		break
+	}
+
+	return uint32(w | (x << 8) | (y << 16) | (z << 24))
+}
+
+func (b *bitbuf) writeByte(value byte, bits uint32) {
+	if bits == 0 {
+		return
+	}
+
+	p := b.pos >> 3
+	used := b.pos & 0x7
+	free := uint32(8 - used)
+	remain := uint32(free - bits)
+
+	if remain >= 0 {
+		mask := byte(0xFF>>free) | (0xFF << (8 - remain))
+		b.buf[p] = byte((b.buf[p] & mask) | (value << used))
+	} else {
+		b.buf[p] = byte(b.buf[p]&(0xFF>>free) | (value << used))
+		b.buf[p+1] = byte((b.buf[p+1] & (0xFF << (bits - free))) | (value >> free))
+	}
+
+	b.pos += bits
+}
+
+func (b *bitbuf) readByte(bits uint32) byte {
+	if bits == 0 {
+		return 0
+	}
+
+	var value byte
+	p := b.pos >> 3
+	used := b.pos % 8
+
+	if used == 0 && bits == 8 {
+		value = b.buf[p]
+	} else {
+		first := b.buf[p] >> used
+		remain := bits - (8 - used)
+
+		if remain < 1 {
+			value = byte(first & (0xFF >> (8 - bits)))
+		} else {
+			second := b.buf[p+1] & (0xFF >> (8 - remain))
+			value = byte(first | (second << (bits - remain)))
+		}
+	}
+
+	b.pos += bits
+	return value
+}
---
+++ bitbuf_test.go
@@ -0,0 +1,105 @@
+package bitbuf
+
+import "testing"
+
+func NewBitbufProperSize(t *testing.T) {
+	buf := NewBitbuf(1000)
+	if buf.Size() != 8000 {
+		t.Error("Expected size of 8000, got ", buf.Size)
+	}
+}
+
+func NewBitbufProperPos(t *testing.T) {
+	buf := NewBitbuf(1000)
+	if buf.Pos() != 0 {
+		t.Error("Expected pos of 0, got ", buf.Size)
+	}
+}
+
+func WriteReadBool(t *testing.T) {
+	buf := NewBitbuf(1)
+	var b bool
+	b = true
+	buf.WriteBool(b)
+	r := buf.ReadBool()
+	if r != b {
+		t.Error("Expected true, got false")
+	}
+}
+
+func WriteReadByte(t *testing.T) {
+	buf := NewBitbuf(1)
+	var b byte
+	b = 123
+	buf.WriteByte(b)
+	r := buf.ReadByte()
+	if r != b {
+		t.Error("Expected 123, got ", r)
+	}
+}
+
+func WriteReadBytePartial(t *testing.T) {
+	buf := NewBitbuf(1)
+	var b byte
+	b = 5
+	buf.WriteBytePart(b, 4)
+	r := buf.ReadBytePart(4)
+	if r != b {
+		t.Error("Expected 5, got ", r)
+	}
+}
+
+func WriteReadInt16(t *testing.T) {
+	buf := NewBitbuf(16)
+	var i int16
+	i = 20912
+	buf.WriteInt16(i)
+	r := buf.ReadInt16()
+	if r != i {
+		t.Error("Expected 20912, got ", r)
+	}
+}
+
+func WriteReadInt16Partial(t *testing.T) {
+	buf := NewBitbuf(11)
+	var i int16
+	i = -1024
+	buf.WriteInt16Part(i, 11)
+	r := buf.ReadInt16Part(11)
+	if r != i {
+		t.Error("Expected -1024, got ", r)
+	}
+}
+
+func WriteReadUint16(t *testing.T) {
+	buf := NewBitbuf(16)
+	var i uint16
+	i = 31234
+	buf.WriteUint16(i)
+	r := buf.ReadUint16()
+	if r != i {
+		t.Error("Expected 31234, got ", r)
+	}
+}
+
+func WriteReadUint16Partial(t *testing.T) {
+	buf := NewBitbuf(12)
+	var i uint16
+	i = 942
+	buf.WriteUint16Part(i, 12)
+	r := buf.ReadUint16Part(12)
+	if r != i {
+		t.Error("Expected 942, got ", r)
+	}
+}
+
+func WriteReadUint32Part(t *testing.T) {
+	buf := NewBitbuf(24)
+	var i uint32
+	i = 1234567
+	buf.WriteUint32Part(i, 24)
+	r := buf.ReadUint32Part(24)
+	if r != i {
+		t.Error("Expected 1234567, got ", r)
+	}
+}
